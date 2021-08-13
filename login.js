6e44562319688354c07e2f926e5553f0b1ef8c7e
FILM <79567980+atthasit2542@users.noreply.github.com>
2021-08-13 21:41:12 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,41 @@
+var jwt=localStorage.getItem("jwt")
+if(jwt!=null){
+    window.location.href="./index.html"
+}
+function login(){
+    const username=document.getElementById("username").value
+    const password=document.getElementById("password").value
+    const xhttp=new XMLHttpRequest()
+    xhttp.open("post","https://www.mecallapi.com/api/login")
+    xhttp.setRequestHeader("content-Type","application/json;charset=UTF-8")
+    xhttp.send(JSON.stringify({
+        "username":username,
+        "password":password
+
+    }))
+    xhttp.onreadystatechange=function(){
+        if(this.readyState==4){
+            const object=JSON.parse(this.responseText)
+            console.log(object)
+            if(object["status"]=="ok"){
+                localStorage.setItem("jwt",object["accessToken"])
+                Swal.fire({
+                    text: object['message'],
+                    icon: 'success',
+                    confirmButtonText: 'OK'
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      window.location.href = './index.html'
+                    }
+                  })
+            }
+            else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Please try again!!'
+                  })
+            }
+        }
+    }
+    return false
+}
